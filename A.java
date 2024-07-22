825c2e7d48775e9c16e2e6cfc4c97f60175d4f86
mhie233 <rohit123dhumal@gmail.com>
2024-07-22 18:42:48 +0530

    add the change

--- A.java
+++ A.java
@@ -3,8 +3,7 @@ public class A {
     public static void main(String [] args){
 
         System.out.println("print anything in new lines");
-
-
+        System.out.println("someone has added a code");
     }
 
 }
